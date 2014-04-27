88da3f8870057d0cbd583a489852d9869c4ecc8b
Sergey Vasilinets <sergeyv@chromium.org>
2014-04-27 16:59:31 +0400

    Fix account request

--- codereview/src/main/java/com/chrome/codereview/IssuesActivity.java
+++ codereview/src/main/java/com/chrome/codereview/IssuesActivity.java
@@ -8,13 +8,29 @@ import com.chrome.codereview.requests.ServerCaller;
 
 public class IssuesActivity extends Activity {
 
+    private static final int REQUEST_LOGIN = 1;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode != REQUEST_LOGIN && resultCode != RESULT_OK) {
+            return;
+        }
+        initializeViews(ServerCaller.from(this));
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ServerCaller serverCaller = ServerCaller.from(this);
         if (serverCaller.getState() != ServerCaller.State.OK) {
-            startActivity(new Intent(this, LoginActivity.class));
+            startActivityForResult(new Intent(this, LoginActivity.class), REQUEST_LOGIN);
+            return;
         }
+        initializeViews(serverCaller);
+    }
+
+    private void initializeViews(ServerCaller serverCaller) {
         getActionBar().setTitle(serverCaller.getAccountName());
         setContentView(R.layout.activity_issue_list);
     }
